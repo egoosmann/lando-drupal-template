65fe757c16004a4f1d6c69ec0fd0b4f40bc15852
Ewout Goosmann <ewout@ephex.nl>
2022-04-12 12:01:02 +0200

    Update compare-template-version.sh

--- compare-template-version.sh
+++ compare-template-version.sh
@@ -6,7 +6,8 @@
 lando_green "Compare template versions..."
 
 current_version=$(awk '/template_version:/{print $NF;}' /app/.lando.base.yml)
-newest_version=$(wget -qO- https://raw.githubusercontent.com/egoosmann/lando-drupal-template/master/version.txt)
+newest_template=$(wget -qO- https://raw.githubusercontent.com/egoosmann/lando-drupal-template/master/.lando.base.yml)
+newest_version=$(wget -qO- $newest_template)
 
 if [[ $current_version = $newest_version ]]
 then
