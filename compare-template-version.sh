38748f8bdc921969dde434a361cc80ee4a2cfa2c
Ewout Goosmann <ewout@ephex.nl>
2022-04-12 13:20:27 +0200

    Update compare-template-version.sh

--- compare-template-version.sh
+++ compare-template-version.sh
@@ -3,29 +3,29 @@
 # Get the lando logger
 . /helpers/log.sh
 
-lando_green "Start .lando.base.yml version check"
+lando_green "Start base file version check"
 
 cd /tmp
 
-lando_check "Dowloading .lando.base.yml from repository"
+lando_check "Dowloading base file from repository"
 wget -q https://raw.githubusercontent.com/egoosmann/lando-drupal-template/master/.lando.base.yml
 
 current_version=$(awk '/template_version:/{print $NF;}' /app/.lando.base.yml)
 newest_version=$(awk '/template_version:/{print $NF;}' /tmp/.lando.base.yml)
 
-lando_check "Compare file versions"
+lando_check "Compare base file versions"
 if [[ $current_version = $newest_version ]]
 then
-  lando_check "Your .lando.base.yml is up to date!"
+  lando_check "Your base file is up to date!"
 else
-  lando_red "Your .lando.base.yml is outdated!"
-  read -p "Do you want to update your .lando.base.yml file? [Y/n] " -r
+  lando_red "Your base file is outdated!"
+  read -p "Do you want to update your base file? [Y/n] " -r
   echo # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-    lando_green "Updating your .lando.base.yml file"
+    lando_green "Updating your base file"
     mv /tmp/.lando.base.yml /app/.lando.base.yml
-    lando_check "Your .lando.base.yml has been updated!"
+    lando_check "Your base file has been updated!"
   fi
 fi
 
