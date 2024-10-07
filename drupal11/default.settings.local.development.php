f4509a7b523ea401fd3703d8eb37bdce0914376e
Ewout Goosmann <ewout@ephex.nl>
2024-10-07 16:02:25 +0200

    Update default.settings.local.development.php

--- drupal11/default.settings.local.development.php
+++ drupal11/default.settings.local.development.php
@@ -32,12 +32,17 @@ $settings['db_port'] = LANDO_INFO['database']['internal_connection']['port'];
 $settings['hash_salt'] = Crypt::hashBase64($settings['db_name'].$settings['db_user'].$settings['db_pass'].$settings['db_host'].$settings['db_port']);
 
 // ### SMTP
+$settings['smtp_on'] = TRUE;
 $settings['smtp_username'] = '';
 $settings['smtp_password'] = '';
 $settings['smtp_host'] = 'mailhog';
 $settings['smtp_port'] = '1025'; // 25, 465, 587, 1025
 $settings['smtp_protocol'] = 'standard'; // standard, ssl, tls
-$settings['smtp_autotls'] = 'off';
+$settings['smtp_autotls'] = FALSE;
+$settings['smtp_from'] = '';
+$settings['smtp_fromname'] = '';
+$settings['smtp_allowhtml'] = TRUE;
+$settings['smtp_debugging'] = TRUE;
 
 // ### Sentry
 $settings['sentry_client_key'] = NULL; // php logging
